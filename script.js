4ed5f42a16798f9890143fdfe6ac8da7ea59f935
Nathan Adams <47441281+nadamspueblo@users.noreply.github.com>
2024-03-05 14:47:22 +0000

    Added reset button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ Once upon a time there was a big dog. One day, the dog went to the park. At the 
 
 function main() {
   addClickEvent("button", showMadLib);
+  addClickEvent("reset", reset);
 }
 
 function showMadLib() {
@@ -24,6 +25,22 @@ function showMadLib() {
   let story = "Once upon a time there was a " + adj1 + " " + animal + ". One day, the " + animal + " went to the " + location + ". At the " + location + ", the dog met a " + adj2 + " " + animal2 + ". The " + animal + " " + verb + "ed the " + animal2 + " up a " + object + ". The " + animal2 + " was very " + emotion + " and " + verb2 + " a " + obj2 + " at the " + animal + ". The " + animal + " " + verb3 + "ed and " + verb3 + "ed, but he couldn't catch the " + animal2 + ". Finally, the " + animal + "'s owner came and took the " + animal + " back to the " + location2 + ". THE END!";
 
   setText(story, "madlib");
+}
+
+function reset() {
+  clearInputValue("animal");
+  clearInputValue("adj1");
+  clearInputValue("location");
+  clearInputValue("animal2");
+  clearInputValue("adj2");
+  clearInputValue("verb");
+  clearInputValue("object");
+  clearInputValue("emotion");
+  clearInputValue("verb2");
+  clearInputValue("object2");
+  clearInputValue("verb3");
+  clearInputValue("location2");
+  setText("Madlib will appear here after the button is pressed", "madlib");
 }
 
 main();
